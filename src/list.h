3d0beaa0bfb6186a0e8ee2249e60b3a22a1adf91
Fansion <Fansionspace@gmail.com>
2015-06-08 21:03:42 +0800

    add list_head(data structure)

---
+++ src/list.h
@@ -0,0 +1,84 @@
+#ifndef __FV_LIST_H
+#define __FV_LIST_H
+
+#ifndef NULL
+#define NULL 0
+#endif
+/*liste head, refer to linux kernel implementation*/
+
+struct list_head {
+    struct list_head *pre, *next;
+};
+
+typedef struct list_head list_head;
+
+#define INIT_LIST_HEAD(ptr) do {                        \
+    struct list_head *_ptr = (struct list_head *)ptr;   \
+    (_ptr)->next = (_ptr); (_ptr)->pre = (_ptr);        \
+} while (0)
+
+/*
+ * Insert a new entry to two consecutive entries
+ */
+static inline void __list_add(struct list_head *_new, struct list_head *pre, struct list_head *next)
+{
+    _new->next = next;
+    next->pre = _new;
+    pre->next = _new;
+    _new->pre = pre;
+};
+
+/*
+ * insert before head
+ */
+static inline void list_add(struct list_head *_new, struct list_head *head)
+{
+    __list_add(_new, head, head->next);
+}
+
+/*
+ * insert after tail, to be modifeid
+ */
+static inline void list_add_tail(struct list_head *_new, struct list_head *head)
+{
+    __list_add(_new, head, head->next);
+}
+
+/*
+ * delete an entry of two consecutive entries
+ * actually combine two list_head
+ */
+static inline void __list_del(struct list_head *pre, struct list_head *next)
+{
+    pre->next = next;
+    next->pre = pre;
+}
+
+static inline void list_del(struct list_head *entry)
+{
+    __list_del(entry->pre, entry->next);
+    entry->next = entry->pre = NULL;
+}
+
+/*
+ * check whether the list is empty
+ */
+static inline int list_empty(struct list_head *head)
+{
+    return (head->next == head) && (head->pre == head);
+}
+
+
+#define offsetof(TYPE, MEMBER)      ((size_t) &((TYPE *)0)->MEMBER)
+#define container_of(ptr, type, member) ({                  \
+    const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
+    (type *)( (char *)__mptr - offsetof(type, member) );    \
+})
+#define list_entry(ptr, type, member)                       \
+    container_of(ptr, type, member)
+#define list_for_each(pos, head)                            \
+    for (pos = (head)->next; pos != (head); pos = pos->next)
+#define list_for_each_prev(pos, head)                       \
+    for (pos = (head)->pre; pos != (head); pos = pos->pre)
+
+#endif
