10ec62d9832134ff652610017d899ea70861542b
Fansion <Fansionspace@gmail.com>
2015-06-14 22:41:34 +0800

    fix bug: client closing socket early will cause SIG_PIPE

--- src/ferver.c
+++ src/ferver.c
@@ -2,6 +2,8 @@
 #include "epoll.h"
 #include "threadpool.h"
 #include <getopt.h>
+#include <signal.h>
+#include <string.h>
 
 #define CONF            "ferver.conf"
 #define VERSION                 "0.0"
@@ -22,7 +24,7 @@ static void usage()
             " -c|--conf <config file> Specify configure, default ./ferver.conf\n"
             " -?|-h|--help\n"
             " -v|--version\n"
-            );
+           );
 }
 
 int main(int argc, char *argv[])
@@ -38,23 +40,23 @@ int main(int argc, char *argv[])
         usage();
         return 0;
     }
-    while((opt = getopt_long(argc, argv, "vc:?h", long_options, &options_index)) != EOF)
+    while ((opt = getopt_long(argc, argv, "vc:?h", long_options, &options_index)) != EOF)
     {
-        switch(opt)
+        switch (opt)
         {
-            case 0:
-                break;
-            case 'c':
-                conf_file = optarg;
-                break;
-            case 'v':
-                printf("%s\n", VERSION);
-                return 0;
-            case ':':
-            case '?':
-            case 'h':
-                usage();
-                return 0;
+        case 0:
+            break;
+        case 'c':
+            conf_file = optarg;
+            break;
+        case 'v':
+            printf("%s\n", VERSION);
+            return 0;
+        case ':':
+        case '?':
+        case 'h':
+            usage();
+            return 0;
         }
     }
     debug("config file: %s", conf_file);
@@ -73,9 +75,20 @@ int main(int argc, char *argv[])
     fv_conf_t cf;
     rc = read_conf(conf_file, &cf, conf_buf, BUFFLEN);
     check(rc == FV_CONF_OK, "read conf err");
-    log_info("root:%s", (char *)cf.root);
-    log_info("port:%d", cf.port);
-    log_info("thread_num:%d", cf.thread_num);
+    debug("root:%s port:%d thread_num:%d", (char *)cf.root, cf.port, cf.thread_num);
+    /*
+     *install signal handle for SIGPIPE
+     * when a fd is closed by remote, writing to this fd will cause system send SIGPIPE to process, which exit the program
+     */
+    struct sigaction sa;
+    memset(&sa, '\0', sizeof(sa));
+    sa.sa_handler = SIG_IGN;        // 忽略信号
+    sa.sa_flags = 0;
+    if (sigaction(SIGPIPE, &sa, NULL))
+    {
+        log_err("install signal handler for SIGPIPE failure");
+        return 0;
+    }
     /*
      *initialize listening socket
      */
@@ -84,32 +97,25 @@ int main(int argc, char *argv[])
     // initialize clientaddr and inlen to solve "accept Invalid argument" bug
     socklen_t inlen = 1;
     memset(&clientaddr, 0, sizeof(struct sockaddr_in));
-
     listenfd = open_listenfd(cf.port);
     rc = make_socket_non_blocking(listenfd);
     check(rc == 0, "make_socket_non_blocking");
-
     /*
      *create epoll and add listenfd to epfd
      */
     int epfd = fv_epoll_create(0);
     struct epoll_event event;
-
     fv_http_request_t *r = (fv_http_request_t *)malloc(sizeof(fv_http_request_t));
     fv_init_request_t(r, listenfd, &cf);
     event.data.ptr = (void *)r;
     event.events = EPOLLIN | EPOLLET;
     fv_epoll_add(epfd, listenfd, &event);
-
     /*
      *create thread pool
      */
     fv_threadpool_t *tp = threadpool_init(cf.thread_num);
-
     while (1)
     {
-        log_info("ferver(fd %d) ready to wait", listenfd);
-        fflush(stdout);
         /* epoll_wait loop */
         int n;
         n = fv_epoll_wait(epfd, events, MAXEVENTS, -1);
@@ -119,16 +125,9 @@ int main(int argc, char *argv[])
         {
             fv_http_request_t *r = (fv_http_request_t *)events[i].data.ptr;
             fd = r->fd;
-            if ((events[i].events & EPOLLERR) ||
-                    (events[i].events & EPOLLERR) ||
-                    (!(events[i].events & EPOLLIN))) {
-                log_err("epoll error(fd %d)", fd);
-                close(fd);
-                continue;
-            }
+
             if (listenfd == fd) {
-                log_info("# ferver(fd %d) ready to accept", listenfd);
-                fflush(stdout);
+                log_info("#--ferver(fd %d) ready to accept--", listenfd);
                 int infd = accept(listenfd, (struct sockaddr *)&clientaddr, &inlen);
                 if (infd == -1) {
                     if ((errno == EAGAIN) || (errno == EWOULDBLOCK)) {
@@ -148,15 +147,22 @@ int main(int argc, char *argv[])
                 event.data.ptr = (void *)r;
                 event.events = EPOLLIN | EPOLLET;
                 fv_epoll_add(epfd, infd, &event);
-                log_info("# end accept");
-                fflush(stdout);
+                log_info("#--end accept--");
             } else {
+                if ((events[i].events & EPOLLERR) ||
+                        (events[i].events & EPOLLERR) ||
+                        (!(events[i].events & EPOLLIN))) {
+                    log_err("epoll error(fd %d)", fd);
+                    close(fd);
+                    continue;
+                }
                 /*
                  do_request(infd);
                  close(infd);
                  */
-                log_info("new data from client(fd %d)", fd);
+                log_info("new task from client(fd %d)", fd);
                 rc = threadpool_add(tp, do_request, (void *)events[i].data.ptr);
+                check(rc == 0, "threadpool_add");
             }
         }
     }
